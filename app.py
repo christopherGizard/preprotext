808d509066402f5e778796e226517742af4fc7fe
christopher <christopher.gizard@etu.univ-lyon1.fr>
2020-08-05 17:43:45 +0200

    Fonction retour à la frame précédente

--- app.py
+++ app.py
@@ -2,19 +2,30 @@
 import tkinter as tk
 #Local import
 from presentationFrame import *
+from selectionFrame import *
 
 class PreProApp(tk.Tk):
 	def __init__(self):
 		tk.Tk.__init__(self)
 		self.frame = None
+		self.previousFrame = None
 		self.switch_frame(PresentationFrame)
 
 	def switch_frame(self,frame_class):
 		new_frame = frame_class(self)
 		if self.frame is not None:
-			self.frame.destroy()
+			self.previousFrame =self.frame
+			self.frame.pack_forget()
 		self.frame = new_frame
 		self.frame.pack()
+		print(self.previousFrame)
+
+	def previous_frame(self):
+		if self.previousFrame is not None:
+			self.frame.pack_forget()
+			self.frame = self.previousFrame
+			self.frame.pack()
+
 
 if __name__ == "__main__":
     app = PreProApp()
