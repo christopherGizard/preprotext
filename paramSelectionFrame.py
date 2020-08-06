e8536bfe351bfcb928908ec4d861caad9b50656f
christopher <christopher.gizard@etu.univ-lyon1.fr>
2020-08-06 18:45:50 +0200

    Ajout du parametres Sentencer (front)

--- paramSelectionFrame.py
+++ paramSelectionFrame.py
@@ -2,12 +2,14 @@ from tkinter import *
 from tkinter.ttk import *
 
 #from selectionFrame import SelectionFrame
+from tools.nlp.sentencer import Sentencer
 
 class ParamSelectionFrame(Frame):
 	def __init__(self, master):
 		Frame.__init__(self, master,width=550,height=450)
 
 		self.parameters = []
+		self.options={}
 
 		#Frame
 		labelF = LabelFrame(self, text="Cleaning parameters")
@@ -51,7 +53,7 @@ class ParamSelectionFrame(Frame):
 	def add_param(self):
 		self.window = Toplevel(self)
 		self.window.grab_set()
-
+		print(self.parameters)
 		#Frame
 		topFrame = Frame(self.window)
 		topFrame.pack(side='top')
@@ -62,19 +64,20 @@ class ParamSelectionFrame(Frame):
 		filFrame = LabelFrame(topFrame,text='Filter')
 		filFrame.pack(side='top',padx=5,pady=5)
 
-		optFrame = LabelFrame(topFrame,text='Options')
-		optFrame.pack(side='bottom', padx=5, pady=5)
+		self.optFrame = LabelFrame(topFrame,text='Options')
+		self.optFrame.pack(side='bottom', padx=5, pady=5)
 
 		#Widget
 		listboxLabel = Label(filFrame,text="Choose your filter")
 		listboxLabel.pack(side="top")
 
-		self.listbox = Listbox(filFrame)
+		self.listbox = Listbox(filFrame,exportselection=False)
 		values = ['Sentencer','Word Tokenizer','Stop Words Suppression','Punctuation Suppression','LowerCase Text','Lemming','Stemming','Custom Words Removal','Custom Regex']
 		for v in values:
 			if v not in self.parameters:
 				self.listbox.insert('end',v)
 		self.listbox.pack(padx=5,pady=5)
+		self.listbox.bind('<<ListboxSelect>>', self.change_options)
 
 
 		addFilterButton = Button(bottomFrame,text='Add Filter',command=self.add_filter)
@@ -83,10 +86,25 @@ class ParamSelectionFrame(Frame):
 		closeButton = Button(bottomFrame,text='Close',command=self.window.destroy)
 		closeButton.pack(side='left',padx=5,pady=5)
 
+		#Sentencer Options
+		self.sentencerFrame = Frame(self.optFrame)
+		libraryListbox = Listbox(self.sentencerFrame,name='library',exportselection=False)
+		for v in Sentencer.AvailableLibrary:
+			libraryListbox.insert('end',v)
+		libraryListbox.pack(side='left',padx=10,pady=10)
+		libraryListbox.bind('<<ListboxSelect>>', lambda event: self.set_options(event, "library"))
+
+		languageListbox = Listbox(self.sentencerFrame,name='language',exportselection=False)
+		for v in Sentencer.AvailableLanguage:
+			languageListbox.insert('end',v)
+		languageListbox.pack(side='right',padx=10,pady=10)
+		languageListbox.bind('<<ListboxSelect>>',  lambda event: self.set_options(event, "language"))
+
+		#Add,delete and reset filters
 	def add_filter(self):
 		if self.listbox.get(ACTIVE) :
 			self.parameters.append(self.listbox.get(ACTIVE))
-			self.tab.insert('', 'end',values=(self.listbox.get(ACTIVE),''))
+			self.tab.insert('', 'end',values=(self.listbox.get(ACTIVE),self.options))
 			self.window.grab_release()
 			self.window.destroy()
 		if self.parameters:
@@ -102,14 +120,36 @@ class ParamSelectionFrame(Frame):
 		if not self.parameters :
 			self.nextButton['state']='disabled'
 
-				
-
 	def reset_filter(self):
-		self.selection=[]
+		self.parameters=[]
 		x = self.tab.get_children()
 		for item in x:
 			self.tab.delete(item)
 		self.nextButton['state']='disabled'
+
+
+	def change_options(self,evt):
+		w = evt.widget
+		index = int(w.curselection()[0])
+		value = w.get(index)
+		print("You selected item {}:{} ".format(index, value))
+		if value == "Sentencer":
+			self.sentencerFrame.pack()
+		else :
+			self.options ={}
+			self.sentencerFrame.pack_forget()
+
+	def set_options(self,evt,name):
+		w=evt.widget
+		index = int(w.curselection()[0])
+		value = w.get(index)
+		self.options[name]=value
+
+
+
+
+
+
 
 
 
